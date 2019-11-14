968edbd43388f135166c12e65a736271487e7588
nkoporec <nejc.koporec@agiledrop.com>
2019-11-14 16:38:14 +0100

    Make stop command as a progress

--- src/commands/base/Stop.ts
+++ src/commands/base/Stop.ts
@@ -1,4 +1,4 @@
-import { workspace, window, Terminal } from 'vscode'
+import * as vscode from 'vscode'
 import Common from '../../Common';
 import Output from '../../utils/Output'
 
@@ -8,16 +8,19 @@ export default class Stop extends Common {
   * Stop the docksal project.
   */
   public static async stopProject() {
-    const command = await this.runCommand({ command: 'fin', args: ['stop'] });
-    if (command) {
-      window.showInformationMessage('Docksal has been stopped');
-      return true;
+    try {
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: 'Docksal: Stopping a project',
+        },
+        () => this.runCommand({ command: 'fin', args: ['stop'] })
+      )
+      vscode.window.showInformationMessage('Docksal: Project has stopped!');
+    } catch (error) {
+      vscode.window.showErrorMessage(error.message)
+      return false
     }
-
-    window.showErrorMessage(
-      `Docksal: Project failed to stop, please see the console output for more info.`
-    )
-    return false;
   }
 
   /**
@@ -25,7 +28,7 @@ export default class Stop extends Common {
   */
   public static async stopVM() {
     let cmd = "fin system stop"
-    let terminal = window.createTerminal('Docksal VM')
+    let terminal = vscode.window.createTerminal('Docksal VM')
     terminal.sendText(cmd)
     terminal.show()
   }
